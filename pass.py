b0e5be9084e4c97bc74514628ac746e29451eedd
Joris <31551856+CSDUMMI@users.noreply.github.com>
2019-02-24 19:51:31 +0000

    Masterpassword Security
    Now the Program will only give the right password if the Masterpassword is provided. And the program will
    check wheter it is the right M.P. if not it'll not even print anything. Only as feature. 
    Due to this even an attacker that has access to the pass.json has can't get your password

--- pass.py
+++ pass.py
@@ -19,17 +19,57 @@
 """
 import json, sys, getpass
 from Crypto.Hash import SHA256
+from Crypto.Hash import SHA512
 
-def passForService(service):
-    passServices = json.load(open("pass.json","r", encoding="utf-8")) # pass.json - see pass.json for an example
-    hash = SHA256.new() # SHA256 Hashcodes are accepted by most services as Password
-    hash.update(passServices[service].encode('utf-8'))
-    hash.update(str(hash.hexdigest())
-                .encode('utf-8')
-                + getpass.getpass("Masterpassword:") # Only if you know the MP you get the real Password
-                .encode('utf-8'))
-    return hash.hexdigest()
+def loadPass():
+    return json.load(open("pass.json","r",encoding="utf-8"))
+
+def passForService(service,passServices):
+    hash = SHA256.new()
+    mp = checkMasterPassword()
+    if type(mp) == type("string"):
+        hash.update(mp.encode('utf-8') + passServices[service].encode('utf-8'))
+        return "Password for " + service + "\n" + str(hash.hexdigest())
+    else:
+        return "Masterpassword wrong!"
+
+def checkMasterPassword():
+    masterpass = getpass.getpass("Masterpassword:")
+    sha512 = SHA512.new(masterpass.encode('utf-8'))
+    masterpassHash = str(sha512.hexdigest())
+    if masterpassHash == passServices['masterpass']:
+        return masterpass
+    else:
+        return False
+
+
+def addMasterPass(passServices):
+    sha512 = SHA512.new(getpass.getpass("New Masterpassword:").encode('utf-8'))
+    passServices['masterpass'] = str(sha512.hexdigest())
+    return passServices
+
+def addNewService(item,hashSeed,passServices):
+    passServices[item] = hashSeed
+
+def savePass(passServices):
+    open("pass.json","w").write(json.dumps(passServices))
 
 if __name__ == '__main__':
-    service = sys.argv[1]
-    print(passForService(service))
+    cmd = sys.argv[1]
+
+    if cmd == 'get':
+        service = sys.argv[2]
+        # output password to service
+        passServices = loadPass()
+        print(passForService(service,passServices))
+    elif cmd == 'add':
+        item = sys.argv[2]
+        passServices = loadPass()
+        if item == 'masterpass':
+            passServices = addMasterPass(passServices)
+        else:
+            hashSeed = sys.argv[3]
+            passServices = addNewService(item,hashSeed,passServices)
+        savePass(passServices)
+    else:
+        print ("Usage: add or get Password")
